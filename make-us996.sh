51e784c37f0b59f6603dfcf6713a81c092fc1e80
Joel Fuster <j@fuster.org>
2018-12-22 14:56:56 -0500

    Fix section mismatch errors

--- make-us996.sh
+++ make-us996.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #MAKE="make O=out KCFLAGS=-Wno-maybe-uninitialized"
-MAKE="make O=out"
+MAKE="make O=out CONFIG_DEBUG_SECTION_MISMATCH=y"
+#MAKE="make O=out"
 $MAKE -j4
--- setup-us996.sh
+++ setup-us996.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #MAKE="make O=out KCFLAGS=-Wno-maybe-uninitialized"
-MAKE="make O=out"
+MAKE="make O=out CONFIG_DEBUG_SECTION_MISMATCH=y"
 mkdir -p out
 make -j4 clean && make -j4 mrproper
 $MAKE clean && $MAKE mrproper && $MAKE lineageos_us996_oxavelar_defconfig
